d00df1da157fa0f09c1cc084d0329e5f4a72adf4
Your Name <jymnguyen02@gmail.com>
2024-09-26 22:14:15 +0700

    add score

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,13 +13,22 @@ public class GameManager : MonoBehaviour
     [Header ("Speed Settings")]
     public float startingSpeed = 0.2f;
     public float speedIncreasePerSecond = 0.02f;
-
+    public float scoreMultiplier = 2f;
     private float currentSpeed;
 
+    [Header ("UI")] public TextMeshProUGUI scoreText;
+    private int highScore = 0;
+    private float currentScore = 0;
     private void Awake()
     {
         Instance = this;
         currentSpeed = startingSpeed;
+
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScore");
+        }
+        UpdateScoreUI();
     }
     private void Update()
     {
@@ -26,6 +36,17 @@ public class GameManager : MonoBehaviour
         {
             currentSpeed += Time.deltaTime * speedIncreasePerSecond;
             floorMeshRenderer.material.mainTextureOffset += new Vector2(currentSpeed * Time.deltaTime, 0);
+            currentScore += currentSpeed * Time.deltaTime * scoreMultiplier;
+            if (Mathf.RoundToInt(currentScore) > highScore)
+            {    
+                highScore = Mathf.RoundToInt(currentScore);
+                PlayerPrefs.SetInt("Highscore", highScore);
+            }
+            UpdateScoreUI();
         }
+    }
+    private void UpdateScoreUI()
+    {
+        scoreText.SetText($"HI {highScore.ToString("D5")} {Mathf.RoundToInt(currentScore).ToString("D5")}");
     }
 }
