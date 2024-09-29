fe59c1613ec03e94dbd27ae77938f43978c54fba
Your Name <jymnguyen02@gmail.com>
2024-09-29 22:23:25 +0700

    add shield

--- Scripts/DinoMovement.cs
+++ Scripts/DinoMovement.cs
@@ -9,21 +9,16 @@ public class DinoMovement : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private Rigidbody2D rb;
 
-    [Header("Audio")] [SerializeField] private AudioSource audio;
+    [Header("Audio")] 
+    [SerializeField] private AudioSource audio;
     [SerializeField] private AudioClip jumpSFX;
     [SerializeField] private AudioClip dieSFX;
-
 
     private bool _isGameStarted = false;
     private bool _isTouchingGround = true;
     private bool _isDead = false;
+    private bool _hasShield = false; // Trạng thái có khiên
 
-    //void Start()
-    //{
-        
-    //}
-
-    // Update is called once per frame
     void Update()
     {
         bool isJumpButtonPressed = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow);
@@ -37,11 +32,8 @@ public class DinoMovement : MonoBehaviour
             {
                 if (_isGameStarted == true && _isTouchingGround == true)
                 {
-                    //Jump
-                    Debug.Log("Jumping...");
                     Jump();
                 }
-
                 else
                 {
                     _isGameStarted = true;
@@ -50,41 +42,54 @@ public class DinoMovement : MonoBehaviour
             }
             else if (isCrouchButtonPressed && _isTouchingGround)
             {
-                //Crouch
+                // Crouch logic
             }
         }
-
 
         dinoAnimator.SetBool("Beginning", _isGameStarted);
         dinoAnimator.SetBool("Folding", isCrouchButtonPressed && _isTouchingGround && !isJumpButtonPressed);
         dinoAnimator.SetBool("Ending", _isDead);
-
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Shield"))
+        {
+            _hasShield = true; // Kích hoạt khiên khi khủng long nhặt được
+            Destroy(other.gameObject); // Xóa khiên khỏi màn chơi
+        }
+        else if (other.CompareTag("Obstacle"))
+        {
+            if (_hasShield)
+            {
+                // Nếu có khiên, phá hủy chướng ngại vật và vô hiệu hóa khiên
+                Destroy(other.gameObject);
+                _hasShield = false; // Khiên bị mất sau khi va chạm
+            }
+            else
+            {
+                _isDead = true; // Khủng long chết nếu không có khiên
+                GameManager.Instance.Ending = true; // Chỉ bật Ending khi khủng long thực sự chết
+                GameManager.Instance.ShowGameEndScreen();
+                audio.clip = dieSFX;
+                audio.Play();
+            }
+        }
+    }
+
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Floor"))
         {
             _isTouchingGround = true;
-        } 
-        else if (other.gameObject.CompareTag("Obstacle"))
-        {
-            //Die
-            _isDead = true;
-           GameManager.Instance.Ending = true;
-
-            GameManager.Instance.ShowGameEndScreen();
-
-            audio.clip = dieSFX;
-            audio.Play();
         }
-    } 
+    }
 
     void Jump()
     {
         rb.AddForce(Vector2.up * jumpForce);
         _isTouchingGround = false;
-
         audio.clip = jumpSFX;
         audio.Play();
     }
