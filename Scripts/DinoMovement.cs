ee234e15068d510715f7609d088dc0b5dd98c534
Trang.BH210851@sis.hust.edu.vn <Trang.BH210851@sis.hust.edu.vn>
2024-09-29 14:28:38 +0700

    full game

--- Scripts/DinoMovement.cs
+++ Scripts/DinoMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,50 +9,83 @@ public class DinoMovement : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private Rigidbody2D rb;
 
+    [Header("Audio")] [SerializeField] private AudioSource audio;
+    [SerializeField] private AudioClip jumpSFX;
+    [SerializeField] private AudioClip dieSFX;
+
+
     private bool _isGameStarted = false;
     private bool _isTouchingGround = true;
+    private bool _isDead = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
     // Update is called once per frame
     void Update()
     {
         bool isJumpButtonPressed = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow);
-        bool isCrouchButtonPressed = Input.GetKey(KeyCode.LeftControl) 
-                                    || Input.GetKey(KeyCode.RightControl) 
+        bool isCrouchButtonPressed = Input.GetKey(KeyCode.LeftControl)
+                                    || Input.GetKey(KeyCode.RightControl)
                                     || Input.GetKey(KeyCode.DownArrow);
-        
-        if (isJumpButtonPressed && _isTouchingGround){
-            if(_isGameStarted == true){
-                //Jump
-                Debug.Log("Jumping...");
-                Jump();
-            }else{
-                _isGameStarted = true;
-                GameManager.Instance.Beginning = true;
-            }
-        } else if (isCrouchButtonPressed){
-            //Crouch
 
+        if (!_isDead)
+        {
+            if (isJumpButtonPressed)
+            {
+                if (_isGameStarted == true && _isTouchingGround == true)
+                {
+                    //Jump
+                    Debug.Log("Jumping...");
+                    Jump();
+                }
+
+                else
+                {
+                    _isGameStarted = true;
+                    GameManager.Instance.Beginning = true;
+                }
+            }
+            else if (isCrouchButtonPressed && _isTouchingGround)
+            {
+                //Crouch
+            }
         }
+
+
         dinoAnimator.SetBool("Beginning", _isGameStarted);
         dinoAnimator.SetBool("Folding", isCrouchButtonPressed && _isTouchingGround && !isJumpButtonPressed);
+        dinoAnimator.SetBool("Ending", _isDead);
+
     }
 
-    private void OnCollisionEnter2D(Collision2D other){
-        if (other.gameObject.CompareTag("Floor")){
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
             _isTouchingGround = true;
-        } else {
-            //game over
+        } 
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            //Die
+            _isDead = true;
+           GameManager.Instance.Ending = true;
+
+            GameManager.Instance.ShowGameEndScreen();
+
+            audio.clip = dieSFX;
+            audio.Play();
         }
     } 
 
-    void Jump(){
+    void Jump()
+    {
         rb.AddForce(Vector2.up * jumpForce);
         _isTouchingGround = false;
+
+        audio.clip = jumpSFX;
+        audio.Play();
     }
 }
--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,41 +12,126 @@ public class GameManager : MonoBehaviour
     [SerializeField] private MeshRenderer floorMeshRenderer;
 
     [HideInInspector]public bool Beginning = false;
+    [HideInInspector] public bool Ending = false;
+
+
     [Header ("Speed Settings")]
+    
     public float startingSpeed = 0.2f;
-    public float speedIncreasePerSecond = 0.02f;
+    public float speedIncreasePerSecond = 0.05f;
     public float scoreMultiplier = 2f;
+    
+    [Header ("UI")] public TextMeshProUGUI scoreText;
+    public GameObject gameEndScreen;
+
+    [Header("Obstacle Spawn")] public float MintimeDelayObstacle = 1f;
+    public float MaxtimeDelayObstacle = 1f;
+    public float obstacleSpeedMultiple = 3f;
+    [Space]
+    public GameObject[] allGroundObstacles;
+    public GameObject[] allFlyObstacles;
+    [Space]
+    public Transform GroundObstaclesSpawnPoint;
+    public Transform FlyObstaclesSpawnPoint;
+
+    private List<GameObject> allCurrentObstacles = new List<GameObject> ();
+
+    [Header("SPX")] [SerializeField] private AudioSource audio;
+    [SerializeField] private AudioClip SFX;
+
     private float currentSpeed;
 
-    [Header ("UI")] public TextMeshProUGUI scoreText;
     private int highScore = 0;
     private float currentScore = 0;
+
+    private float timeNextObstacle = 1f;
+
     private void Awake()
     {
         Instance = this;
+
         currentSpeed = startingSpeed;
 
         if (PlayerPrefs.HasKey("HighScore"))
         {
             highScore = PlayerPrefs.GetInt("HighScore");
         }
+
         UpdateScoreUI();
     }
+
+    public void ShowGameEndScreen()
+    {
+        gameEndScreen.SetActive(true);
+    }
+
     private void Update()
     {
-        if (Beginning)
+        if (Beginning && !Ending)
         {
+            timeNextObstacle -= Time.deltaTime * currentSpeed;
+
+            if (timeNextObstacle <= 0)
+            {
+                timeNextObstacle = UnityEngine.Random.Range(MintimeDelayObstacle, MaxtimeDelayObstacle);
+                //spawn a new obstacle
+                if (currentScore >= 50)
+                {
+                    //randomly spawn ground or air obstacles
+                    if (UnityEngine.Random.value > 0.8f) //20% chance happening
+                    {
+                        GameObject newObstacle = Instantiate(allFlyObstacles[UnityEngine.Random.Range(0, allFlyObstacles.Length)], FlyObstaclesSpawnPoint.position, Quaternion.identity);
+                        allCurrentObstacles.Add(newObstacle);
+                    }
+                    else
+                    {
+                        GameObject newObstacle = Instantiate(allGroundObstacles[UnityEngine.Random.Range(0, allGroundObstacles.Length)], GroundObstaclesSpawnPoint.position, Quaternion.identity);
+                        allCurrentObstacles.Add(newObstacle);
+                    }
+                }
+                else
+                {
+                    //Randomly spawn only ground ostacles
+                    GameObject newObstacle = Instantiate(allGroundObstacles[UnityEngine.Random.Range(0, allGroundObstacles.Length)], GroundObstaclesSpawnPoint.position, Quaternion.identity);
+                    allCurrentObstacles.Add(newObstacle);
+                }
+            }
+
+            foreach (GameObject _obstacle in allCurrentObstacles)
+            {
+                _obstacle.transform.Translate(new Vector3(-currentSpeed * Time.deltaTime * obstacleSpeedMultiple, 0, 0));
+            }
+
+
             currentSpeed += Time.deltaTime * speedIncreasePerSecond;
-            floorMeshRenderer.material.mainTextureOffset += new Vector2(currentSpeed * Time.deltaTime, 0);
+            floorMeshRenderer.material.mainTextureOffset += new Vector2(currentSpeed * Time.deltaTime, y:0);
+
+            //if (Mathf.RoundToInt(currentScore += currentSpeed * Time.deltaTime * scoreMultiplier) > Mathf.RoundToInt(currentScore)  && )
+            // OR
+            int lastCurrentScore = Mathf.RoundToInt(currentScore);
+
             currentScore += currentSpeed * Time.deltaTime * scoreMultiplier;
+
+            if (Mathf.RoundToInt(currentScore) > lastCurrentScore && Mathf.RoundToInt(currentScore) % 1000 == 0)
+            {
+                audio.clip = SFX;
+                audio.Play();
+            }
+            
             if (Mathf.RoundToInt(currentScore) > highScore)
             {    
                 highScore = Mathf.RoundToInt(currentScore);
                 PlayerPrefs.SetInt("Highscore", highScore);
             }
+
             UpdateScoreUI();
         }
     }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     private void UpdateScoreUI()
     {
         scoreText.SetText($"HI {highScore.ToString("D5")} {Mathf.RoundToInt(currentScore).ToString("D5")}");
